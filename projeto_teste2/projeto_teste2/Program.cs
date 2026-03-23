8e86e440d5f6b4a6dfa6f669e4a38bb22e6beeea
Shuropa <d2026006049@unifei.edu.br>
2026-03-22 22:48:52 -0300

    Add files via upload

---
+++ projeto_teste2/projeto_teste2/Program.cs
@@ -0,0 +1,32 @@
+﻿Random random = new Random();
+
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if ((daysUntilExpiration <= 10) && (daysUntilExpiration > 5))
+{
+    Console.WriteLine($"Your subscription will expire soon in {daysUntilExpiration}. Renew now!");
+}
+
+else if ((daysUntilExpiration <= 5) && (daysUntilExpiration > 1))
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage += 10;
+    Console.WriteLine($"Renew now and save {discountPercentage}%");
+}
+
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage += 20;
+    Console.WriteLine($"Renew now and save {discountPercentage}%");
+}
+
+else if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else
+{
+    Console.WriteLine($"Your subscription is active. It will expire in {daysUntilExpiration} days.");
+}
---
+++ projeto_teste3/projeto_teste3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Globalization;
+using System.Numerics;
+
+//string[] fraudulentOrderIDs = new string[3];
+
+//fraudulentOrderIDs[0] = "A123";
+//fraudulentOrderIDs[1] = "B456";
+//fraudulentOrderIDs[2] = "C789";
+
+string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassing First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.\n\n");
+
+string[] names = ["Bob", "Connie", "Fred", "Sally\n\n"];
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+int total = 0;
+int bin = 0;
+
+int[] numbers = [100, 200, 250, 375, 500];
+foreach (int number in numbers)
+{
+    total += number;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {number} items (Running total: {total})");
+}
+Console.WriteLine($"We have {total} items in inventory.\n\n");
+
+string[] inventoryIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+Console.WriteLine($"All inventory IDs: ");
+foreach (string ID in inventoryIDs)
+{
+    Console.WriteLine(ID);
+}
+foreach (string ID in inventoryIDs)
+{
+    if (ID.StartsWith("B"))
+    {
+        Console.WriteLine("ID starts with B: " + ID);
+    }
+}
+
+foreach (string ID in inventoryIDs)
+{
+    if (ID.Length > 4)
+    {
+        Console.WriteLine("ID length greater than 4: " + ID);
+    }
+}
